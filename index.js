c50c096252e7b9b4483acb5063118a07f9dfc3ec
David Fellows <dfellows12@gmail.com>
2019-04-22 02:07:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function takeANumber(line, name) {
-  
+  line.push(name);
+  `Welcome, ${name}. You are number ${line.length} in line.`;
+}
+
+function nowServing(line) {
+  if (line.length === 0) {
+    return 'There is nobody waiting to be served!';
+  }
+  return `Now serving ${line.unshift()}`;
 }
